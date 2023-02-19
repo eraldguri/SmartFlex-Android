e18d3dc73192b736135e17068b89802167626d3b
Erald Guri <44472105+eraldguri@users.noreply.github.com>
2023-02-19 17:44:58 +0100

    Changing category buttons style.

--- app/src/main/java/com/erald_guri/smartflex_android/holders/ButtonViewHolder.kt
+++ app/src/main/java/com/erald_guri/smartflex_android/holders/ButtonViewHolder.kt
@@ -1,16 +1,10 @@
 package com.erald_guri.smartflex_android.holders
 
-import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
-import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.ShapeDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.erald_guri.smartflex_android.R
 import com.erald_guri.smartflex_android.data.model.ButtonModel
 import com.erald_guri.smartflex_android.databinding.LayoutButtonListBinding
 import com.erald_guri.smartflex_android.interfaces.OnItemClickListener
-import com.erald_guri.smartflex_android.interfaces.OnRecyclerItemClickListener
 
 class ButtonViewHolder(
     private val binding: LayoutButtonListBinding,
@@ -23,35 +17,16 @@ class ButtonViewHolder(
             setCompoundDrawablesWithIntrinsicBounds(item.icon, null, null, null)
             compoundDrawablePadding = 16
 
-            binding.root.setBackgroundResource(R.drawable.button_round_selected)
-            val backgroundDrawable = binding.root.background
-            if (backgroundDrawable is ShapeDrawable) {
-                backgroundDrawable.paint.color = Color.parseColor(item.color)
-            } else if (backgroundDrawable is GradientDrawable) {
-               backgroundDrawable.setColor(Color.parseColor(item.color))
-            }
-
+            binding.root.setBackgroundResource(R.drawable.button_round_corners_unselected)
         }
     }
 
     fun setSelectedState(item: ButtonModel) {
-        binding.root.setBackgroundResource(R.drawable.button_round_corners_unselected)
-        val backgroundDrawable = binding.root.background
-        if (backgroundDrawable is ShapeDrawable) {
-            backgroundDrawable.paint.color = Color.parseColor(item.color)
-        } else if (backgroundDrawable is GradientDrawable) {
-            backgroundDrawable.setColor(Color.parseColor(item.color))
-        }
+        binding.root.setBackgroundResource(R.drawable.button_round_selected)
     }
 
     fun setUnSelectedState(item: ButtonModel) {
-        binding.root.setBackgroundResource(R.drawable.button_round_selected)
-        val backgroundDrawable = binding.root.background
-        if (backgroundDrawable is ShapeDrawable) {
-            backgroundDrawable.paint.color = Color.parseColor(item.color)
-        } else if (backgroundDrawable is GradientDrawable) {
-            backgroundDrawable.setColor(Color.parseColor(item.color))
-        }
+        binding.root.setBackgroundResource(R.drawable.button_round_corners_unselected)
     }
 
 }
