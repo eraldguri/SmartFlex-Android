ce992d6996d3d4ce448514c88eea7fc4868939f5
Erald Guri <44472105+eraldguri@users.noreply.github.com>
2023-03-30 15:25:18 +0200

    populate contact view

--- app/src/main/java/com/erald_guri/smartflex_android/ui/contacts/ContactViewFragment.kt
+++ app/src/main/java/com/erald_guri/smartflex_android/ui/contacts/ContactViewFragment.kt
@@ -2,11 +2,15 @@ package com.erald_guri.smartflex_android.ui.contacts
 
 import android.annotation.SuppressLint
 import android.graphics.Typeface
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.erald_guri.smartflex_android.R
 import com.erald_guri.smartflex_android.base.BaseFragment
 import com.erald_guri.smartflex_android.databinding.FragmentContactViewBinding
@@ -32,96 +36,56 @@ class ContactViewFragment : BaseFragment<FragmentContactViewBinding>(
 
     @SuppressLint("SetTextI18n")
     private fun fetchContact(contactId: Int) {
-        val italicTypeface = Typeface.defaultFromStyle(Typeface.ITALIC)
         binding.apply {
             viewModel.getContactById(contactId)
             viewModel.contact.observe(viewLifecycleOwner) {
+                val imageUri = Uri.parse(it.photoPath)
+                photo.imageTintMode = null
+                if (it.photoPath.isNotEmpty()) {
+                    Glide.with(requireContext())
+                        .load("file:" + imageUri)
+                        .override(500, 500)
+                        .into(photo)
+                } else {
+                    Glide.with(requireContext())
+                        .load(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_emoji_emotions_24))
+                        .override(500, 500)
+                        .into(photo)
+                }
                 tvName.text = "${it.firstName} ${it.lastName}"
-                if (it.email.isNotEmpty()) {
-                    tvEmail.text = it.email
-                } else {
-                    tvEmail.typeface = italicTypeface
-                    tvEmail.text = getString(R.string.email_not_set)
-                }
 
-                if (it.phone.isNotEmpty()) {
-                    tvPhone.text = it.phone
-                } else {
-                    tvPhone.typeface = italicTypeface
-                    tvPhone.text = getString(R.string.phone_not_set)
-                }
-
-                if (it.title.isNotEmpty()) {
-                    tvTitleValue.text = it.title
-                } else {
-                    tvTitleValue.typeface = italicTypeface
-                    tvTitleValue.text = getString(R.string.title_not_set)
-                }
-
-                if (it.company.isNotEmpty()) {
-                    tvCompanyValue.text = it.company
-                } else {
-                    tvCompanyValue.typeface = italicTypeface
-                    tvCompanyValue.text = getString(R.string.company_not_set)
-                }
-
-                if (it.accountName.isNotEmpty()) {
-                    tvAccountNameValue.text = it.accountName
-                } else {
-                    tvAccountNameValue.typeface = italicTypeface
-                    tvAccountNameValue.text = getString(R.string.account_name_not_set)
-                }
-
-                if (it.accountName.isNotEmpty()) {
-                    tvVendorNameValue.text = it.accountName
-                } else {
-                    tvVendorNameValue.typeface = italicTypeface
-                    tvVendorNameValue.text = getString(R.string.vendor_name_not_set)
-                }
-
-                if (it.leadSource.isNotEmpty()) {
-                    tvVendorLeadSource.text = it.accountName
-                } else {
-                    tvVendorLeadSource.typeface = italicTypeface
-                    tvVendorLeadSource.text = getString(R.string.lead_source_not_set)
-                }
-
-                if (it.dateOfBirth.isNotEmpty()) {
-                    tvBirthdayValue.text = it.accountName
-                } else {
-                    tvBirthdayValue.typeface = italicTypeface
-                    tvBirthdayValue.text = getString(R.string.birthday_not_set)
-                }
-
-                if (it.phone.isNotEmpty()) {
-                    tvPhoneValue.text = it.phone
-                } else {
-                    tvPhoneValue.typeface = italicTypeface
-                    tvPhoneValue.text = getString(R.string.phone_not_set)
-                }
-
-                if (it.otherPhone.isNotEmpty()) {
-                    tvOtherPhoneValue.text = it.otherPhone
-                } else {
-                    tvOtherPhoneValue.typeface = italicTypeface
-                    tvOtherPhoneValue.text = getString(R.string.phone_not_set)
-                }
-
-                if (it.mobile.isNotEmpty()) {
-                    tvMobileValue.text = it.mobile
-                } else {
-                    tvMobileValue.typeface = italicTypeface
-                    tvMobileValue.text = getString(R.string.phone_not_set)
-                }
-
-                if (it.secondaryEmail.isNotEmpty()) {
-                    tvSecondaryEmailValue.text = it.secondaryEmail
-                } else {
-                    tvSecondaryEmailValue.typeface = italicTypeface
-                    tvSecondaryEmailValue.text = getString(R.string.phone_not_set)
-                }
-
+                tableValues(it.email,           tvEmail,                    getString(R.string.email))
+                tableValues(it.phone,           tvPhone,                    getString(R.string.phone))
+                tableValues(it.title,           tvTitleValue,               getString(R.string.title))
+                tableValues(it.accountName,     tvAccountNameValue,         getString(R.string.account_name))
+                tableValues(it.company,         tvCompanyValue,             getString(R.string.company))
+                tableValues(it.vendorName,      tvVendorNameValue,          getString(R.string.vendor_name))
+                tableValues(it.leadSource,      tvVendorLeadSource,         getString(R.string.lead_source))
+                tableValues(it.dateOfBirth,     tvBirthdayValue,            getString(R.string.date_of_birth))
+                tableValues(it.phone,           tvPhoneValue,               getString(R.string.phone))
+                tableValues(it.otherPhone,      tvOtherPhoneValue,          getString(R.string.other_phone))
+                tableValues(it.mobile,          tvMobileValue,              getString(R.string.mobile))
+                tableValues(it.secondaryEmail,  tvSecondaryEmailValue,      getString(R.string.secondary_email))
+                tableValues(it.street,          tvStreetValue,              getString(R.string.street))
+                tableValues(it.city,            tvCityValue,                getString(R.string.city))
+                tableValues(it.zipCode,         tvZipCodeValue,             getString(R.string.zip_code))
+                tableValues(it.otherZipCode,    tvOtherZipCodeValue,        getString(R.string.other_zip_code))
+                tableValues(it.state,           tvStateValue,               getString(R.string.state))
+                tableValues(it.otherStreet,     tvOtherStreetValue,         getString(R.string.other_street))
+                tableValues(it.otherCity,       tvOtherCityValue,           getString(R.string.other_city))
+                tableValues(it.otherState,      tvOtherStateValue,          getString(R.string.other_state))
+                tableValues(it.otherCountry,    tvOtherCountryValue,        getString(R.string.other_country))
             }
+        }
+    }
+
+    private fun tableValues(item: String, value: TextView, field: String) {
+        val italicTypeface = Typeface.defaultFromStyle(Typeface.ITALIC)
+        if (item.isNotEmpty()) {
+            value.text = item
+        } else {
+            value.typeface = italicTypeface
+            value.text = field + " " + getString(R.string.is_not_set)
         }
     }
 
